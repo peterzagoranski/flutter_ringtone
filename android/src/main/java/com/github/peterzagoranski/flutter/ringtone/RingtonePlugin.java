9cb35b35f8937de1f5793170860832a2b43f1dee
Peter Zagoranski <peter.zagoranski@gmail.com>
2019-05-29 13:48:50 +0200

    Issues-1: Set volume to MAX

--- android/src/main/java/com/github/peterzagoranski/flutter/ringtone/RingtonePlugin.java
+++ android/src/main/java/com/github/peterzagoranski/flutter/ringtone/RingtonePlugin.java
@@ -1,5 +1,6 @@
 package com.github.peterzagoranski.flutter.ringtone;
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -15,6 +16,12 @@ public class RingtonePlugin implements MethodCallHandler {
   private RingtonePlugin(Registrar registrar){
     this.ringtone = RingtoneManager.getRingtone(registrar.context().getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
     this.ringtone.setStreamType(AudioManager.STREAM_ALARM);
+
+    final AudioManager manager = (AudioManager)registrar.context().getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+
+    if (null != manager) {
+      manager.setStreamVolume(AudioManager.STREAM_ALARM, manager.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+    }
   }
 
   /** Plugin registration. */
